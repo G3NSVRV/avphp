3618a0081f969a0d7f12b8a847ee8052b960f047
[G3NSVRV] <jonathangarces@live.co.uk>
2016-06-01 18:03:24 -0300

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,7 +1,10 @@
 #!/bin/bash
 #
-
-rm -f /etc/avphp/
-mkdir /etc/avphp/
-cp ./* /etc/avphp/
-if (cat /var/spool/cron/root | grep avphp); then sed -i 's/^.*.avphp.*/0 18 \* \* 1,3,5 sh \/etc\/avphp\/run.sh/g' /var/spool/cron/root; else echo "0 18 * * 1,3,5 sh /etc/avphp/run.sh" >> /var/spool/cron/root; fi
+if [ "$(whoami)" == "root" ]; then
+rm -f /etc/avphp/;
+mkdir /etc/avphp/;
+cp ./* /etc/avphp/;
+if (cat /var/spool/cron/root | grep avphp); then sed -i 's/^.*.avphp.*/0 18 \* \* 1,3,5 sh \/etc\/avphp\/run.sh/g' /var/spool/cron/root; else echo "0 18 * * 1,3,5 sh /etc/avphp/run.sh" >> /var/spool/cron/root; fi;
+else
+echo "Debe iniciar la instalacion como superusuario";
+fi
