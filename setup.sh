9a54c3453628a7165af56571c64a16906df352e7
[G3NSVRV] <jonathangarces@live.co.uk>
2016-06-02 10:46:18 -0300

    Update setup.sh
    it's a directory durr

--- setup.sh
+++ setup.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 if [ "$(whoami)" = "root" ]; then
-rm -f /etc/avphp/;
+rm -rf /etc/avphp/;
 mkdir /etc/avphp/;
 cp ./* /etc/avphp/;
 if (cat /var/spool/cron/root | grep avphp); then sed -i 's/^.*.avphp.*/0 18 \* \* 1,3,5 sh \/etc\/avphp\/run.sh/g' /var/spool/cron/root; else echo "0 18 * * 1,3,5 sh /etc/avphp/run.sh" >> /var/spool/cron/root; fi;
